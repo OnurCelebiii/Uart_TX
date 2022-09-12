23a0f5a6fbac1d2c232c32c16c37fab04ba98871
OnurCelebiii <112555121+OnurCelebiii@users.noreply.github.com>
2022-09-12 15:50:58 +0300

    Update top_uart_tx.vhd

--- top_uart_tx.vhd
+++ top_uart_tx.vhd
@@ -1,87 +1,155 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- Create Date: 22.08.2022 14:29:14
--- Design Name: Uart TX Top Module with clk_wizard 
-----------------------------------------------------------------------------------
-
+--uart tx module--
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity top_uart_tx is 
+entity uart_tx is
 
-    Port (top_i_rst               :    in       std_logic                         ;
-          top_i_start_byte        :    in       std_logic                         ;
-          top_o_tx                :    out      std_logic                         ;
-          --s_top_i_tx_data                   :     in  std_logic_vector(7 downto 0)               ;
-          top_clk_in1_p           :    in       std_logic                         ;
-          top_clk_in1_n           :    in       std_logic)                        ;
+    Port (i_clk         :        in std_logic                           ;
+          i_tx_data     :        in std_logic_vector (7 downto 0)       ;
+          i_rst         :        in std_logic                           ;
+          i_start_byte  :        in std_logic                           ;
+          o_tx_reg      :        out std_logic_vector (7 downto 0)      ;
+          o_tx          :        out std_logic)                         ;
+          
+end uart_tx;
 
+architecture Behavioral of uart_tx is
 
-end top_uart_tx;
+signal clk_counter      :       integer range 0 to 105                  ;
+signal d_clk            :       std_logic := '0'                        ;
+signal control          :       integer   := 104                        ;
+signal clk_divider      :       integer range 0 to 3                    ;
+signal s_i_tx_data      :       std_logic_vector (7 downto 0)           ;  
+signal s_i_start_byte   :       std_logic                               ;
+signal s_o_tx           :       std_logic:='0'                          ;
+type state is (idle, start, data, stop)                                 ;
+signal uart_txx         :       state                                   ;
+signal i                :       integer :=  0                           ;
+signal s_o_tx_reg       :       std_logic_vector (7 downto 0)           ;
 
-architecture Behavioral of top_uart_tx is
-
-signal s_clk_wiz_out                     :       std_logic                                       ;
-signal s_top_i_tx_data                   :       std_logic_vector(7 downto 0);
-
-component uart_tx is    
-
-    Port (i_clk                 :        in      std_logic                       ;
-          i_tx_data             :        in      std_logic_vector (7 downto 0)   ;
-          i_rst                 :        in      std_logic                       ;
-          i_start_byte          :        in      std_logic                       ;
-          o_tx                  :        out     std_logic)                      ;
-
-end component;    
-
-component clk_wiz_0
-
-    port
-     (clk_out1          :           out    std_logic       ;
-      clk_in1_p         :           in     std_logic       ;
-      clk_in1_n         :           in     std_logic)      ;
-
-end component;
-------------- Begin Cut here for COMPONENT Declaration ------ COMP_TAG
-COMPONENT vio_0
-  PORT (
-    clk : IN STD_LOGIC;
-    probe_out0 : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
-  );
-END COMPONENT;
--- COMP_TAG_END ------ End COMPONENT Declaration ------------
-
--- The following code must appear in the VHDL architecture
--- body. Substitute your own instance name and net names.
-
-------------- Begin Cut here for INSTANTIATION Template ----- INST_TAG
 
 begin
-your_instance_name : vio_0
-  PORT MAP (
-    clk => s_clk_wiz_out,
-    probe_out0 => s_top_i_tx_data
-  );
 
-tx : uart_tx port map (
+process (i_clk) begin
 
-i_clk            =>     s_clk_wiz_out         ,
-i_tx_data        =>     s_top_i_tx_data       ,
-i_rst            =>     top_i_rst             ,
-i_start_byte     =>     top_i_start_byte      ,
-o_tx             =>     top_o_tx)             ;
+    if rising_edge(i_clk) then
+        if clk_divider = 3 then 
 
+            d_clk           <=      not d_clk        ;
+            clk_divider     <=      0                ;
 
-clk_wizard : clk_wiz_0 port map ( 
+        else 
 
-   clk_out1       =>   s_clk_wiz_out        ,
-   clk_in1_p      =>   top_clk_in1_p        ,
-   clk_in1_n      =>   top_clk_in1_n)       ;
+            clk_divider     <=      clk_divider + 1  ;
+
+        end if;
+    
+    end if;
+
+end process;
 
 
+process (d_clk, i_rst, i_tx_data, i_start_byte, s_o_tx) begin
 
+      
+
+if rising_edge(d_clk) then 
+    o_tx                    <=          s_o_tx                  ;                
+    s_i_tx_data             <=          i_tx_data               ;      
+    s_i_start_byte          <=          i_start_byte            ;
+    o_tx_reg                <=          s_o_tx_reg              ;
+    
+    if i_rst = '1' then
+
+        clk_counter         <=          0                       ;
+        o_tx                <=          '0'                     ;
+        s_i_start_byte      <=          '1'                     ;
+        i                   <=          0                       ;
+        uart_txx            <=          idle                    ;
+        s_o_tx              <=          '0'                     ;
+        
+    else 
+
+        case uart_txx is 
+
+                when idle =>    if  s_i_start_byte      =      '1'              then 
+
+                                    uart_txx            <=      start               ;
+                                    clk_counter         <=      0                   ;
+                                    s_o_tx              <=      '0'                 ;
+
+                                else
+                                    
+                                    s_o_tx              <=      '1'                 ;
+                                    uart_txx            <=      idle                ;
+
+                                end if; 
+
+
+                when start =>   if  clk_counter         >=      control  then
+
+                                    s_o_tx              <=     '0'                   ;
+                                    clk_counter         <=      0                    ;
+                                    uart_txx            <=      data                 ;
+                                    s_i_start_byte      <=      '0'                  ;
+ 
+                                else
+
+                                    clk_counter         <=      clk_counter + 1     ;
+                                    s_o_tx              <=      '1'                 ;
+
+                                end if; 
+
+
+                when data =>    if  clk_counter      >=      control    then 
+
+                                    if i             <       7           then
+
+                                       i             <=      i   +   1              ;
+                                       clk_counter   <=      0                      ;
+                                       s_o_tx        <=      '0'                    ;
+
+                                    else
+
+                                        i            <=      0                      ;
+                                        uart_txx     <=      stop                   ;
+                                        clk_counter  <=      0                      ;
+
+                                    end if;
+
+                                else
+
+                                    s_o_tx           <=      s_i_tx_data(i)         ;
+                                    s_o_tx_reg (i)   <=      s_o_tx                 ;
+                                    clk_counter      <=      clk_counter +1         ;
+
+                                end if;
+
+
+                when stop =>    if clk_counter       >=      control then 
+
+                                    s_o_tx           <=      '1'                    ;
+                                    clk_counter      <=      0                      ;
+                                    uart_txx         <=      idle                   ;
+                                    s_i_start_byte   <=      '0'                    ;
+
+                                else
+
+                                    clk_counter      <=      clk_counter + 1        ;
+                                    s_i_start_byte   <=      '0'                    ;
+                                    s_o_tx           <=      '1'                    ;
+
+                                end if;
+
+
+                when others =>
+
+       end case;
+    end if;
+end if;
+
+end process;
 
 end Behavioral;
